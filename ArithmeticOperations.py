2e800fb607ddc3382d41639b776b0efe09a1e050
256018 <rohangupta.1106@gmail.com>
2021-04-18 12:27:55 +0530

    Performing Arithmetic Operations

---
+++ ArithmeticOperations.py
@@ -0,0 +1,10 @@
+# Arithmetic operations
+# Taking input
+print("Enter first number")
+a = int(input())
+print("Enter second number")
+b = int(input())
+print("Addition of two numbers ", a, "and", b, "is", (a + b))
+print("Subtraction of two numbers ", a, "and", b, "is", (a - b))
+print("Multiplication of two numbers ", a, "and", b, "is", (a * b))
+print("Division of two numbers ", a, "and", b, "is", (a // b))
