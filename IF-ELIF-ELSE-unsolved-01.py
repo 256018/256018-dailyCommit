169cbd90c436a3bf1daa00bc8cba9e3c42a5c1f3
Rohan Gupta <47993921+256018@users.noreply.github.com>
2021-04-22 22:48:39 +0530

    Solved first problem of if else

---
+++ IF-ELIF-ELSE-unsolved-01.py
@@ -0,0 +1,11 @@
+n=input()
+if n == "lol":
+    print("laughing out loud")
+elif n == "rofl":
+    print("rolling on the floor laughing")
+elif n == "lmk":
+    print("let me know")
+elif n == "smh":
+    print("shaking my head")
+else:
+    print("not recognizable")
